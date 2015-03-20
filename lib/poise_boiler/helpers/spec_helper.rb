6e2db4968af1f5baf8716279acc7261a7b6372a7
Noah Kantrowitz <noah@coderanger.net>
2015-03-19 17:24:37 -0700

    Move the spec helper to the new helper system.
    This allows getting the gemspec to pass down to Halite. Also allows
    easier config for the halite spec helper include.

---
+++ lib/poise_boiler/helpers/spec_helper.rb
@@ -0,0 +1,71 @@
+#
+# Copyright 2015, Noah Kantrowitz
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'halite/helper_base'
+
+
+module PoiseBoiler
+  module Helpers
+    class SpecHelper < Halite::HelperBase
+      def install
+        require 'rspec'
+        require 'rspec/its'
+        require 'simplecov'
+
+        # Check for coverage stuffs
+        formatters = []
+        if ENV['CODECLIMATE_REPO_TOKEN']
+          require 'codeclimate-test-reporter'
+          formatters << CodeClimate::TestReporter::Formatter
+        end
+
+        if ENV['CODECOV_TOKEN']
+          require 'codecov'
+          formatters << SimpleCov::Formatter::Codecov
+        end
+
+        unless formatters.empty?
+          SimpleCov.formatters = formatters
+        end
+
+        SimpleCov.start do
+          # Don't get coverage on the test cases themselves.
+          add_filter '/spec/'
+          add_filter '/test/'
+          # Codecov doesn't automatically ignore vendored files.
+          add_filter '/vendor/'
+        end
+
+        RSpec.configure do |config|
+          # Basic configuraiton
+          config.run_all_when_everything_filtered = true
+          config.filter_run(:focus)
+
+          # Run specs in random order to surface order dependencies. If you find an
+          # order dependency and want to debug it, you can fix the order by providing
+          # the seed, which is printed after each run.
+          #     --seed 1234
+          config.order = 'random'
+
+          unless options['no_halite']
+            require 'halite/spec_helper'
+            config.include Halite::SpecHelper(gemspec)
+          end
+        end
+      end # /def install
+    end
+  end
+end
--- lib/poise_boiler/spec_helper.rb
+++ lib/poise_boiler/spec_helper.rb
@@ -14,48 +14,5 @@
 # limitations under the License.
 #
 
-require 'rspec'
-require 'rspec/its'
-require 'simplecov'
-require 'poise_boiler'
-
-# Check for coverage stuffs
-formatters = []
-if ENV['CODECLIMATE_REPO_TOKEN']
-  require 'codeclimate-test-reporter'
-  formatters << CodeClimate::TestReporter::Formatter
-end
-
-if ENV['CODECOV_TOKEN']
-  require 'codecov'
-  formatters << SimpleCov::Formatter::Codecov
-end
-
-unless formatters.empty?
-  SimpleCov.formatters = formatters
-end
-
-SimpleCov.start do
-  # Don't get coverage on the test cases themselves.
-  add_filter '/spec/'
-  add_filter '/test/'
-  # Codecov doesn't automatically ignore vendored files.
-  add_filter '/vendor/'
-end
-
-RSpec.configure do |config|
-  # Basic configuraiton
-  config.run_all_when_everything_filtered = true
-  config.filter_run(:focus)
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = 'random'
-
-  if PoiseBoiler.include_halite_spec_helper
-    require 'halite/spec_helper'
-    config.include Halite::SpecHelper
-  end
-end
+require 'poise_boiler/helpers/spec_helper'
+::PoiseBoiler::Helpers::SpecHelper.install
