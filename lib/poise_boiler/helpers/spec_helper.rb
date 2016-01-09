29ad72c1347e37c1a7577a130c55576591817259
Noah Kantrowitz <noah@coderanger.net>
2016-01-09 01:28:18 -0800

    Set the spec log level based on verbose/debug.

--- lib/poise_boiler/helpers/spec_helper.rb
+++ lib/poise_boiler/helpers/spec_helper.rb
@@ -72,6 +72,14 @@ module PoiseBoiler
             config.include Halite::SpecHelper(gem_name ? gemspec : nil)
             # Hide the spec helper from RSpec traces by default.
             config.backtrace_exclusion_patterns << %r{/halite/spec_helper}
+            # In verbose mode, set Chef to log level debug by default.
+            if ENV['VERBOSE']
+              config.before { chefspec_options[:log_level] ||= :debug }
+            end
+            # In debug mode, enable Poise log spew.
+            if ENV['DEBUG']
+              config.before { default_attributes[:poise_debug] ||= true }
+            end
           end
         end
       end # /def install
