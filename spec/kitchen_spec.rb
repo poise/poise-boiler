95cbbf7e90e6803f16ece03df1bcb3126736870b
Noah Kantrowitz <noah@coderanger.net>
2016-01-31 19:05:11 -0800

    Update the tests.
    Can't test the chef install bits now that it's in the Dockerfile :-/ Also
    put some of the assertions in shared blocks to speed things up.

--- spec/kitchen_spec.rb
+++ spec/kitchen_spec.rb
@@ -19,26 +19,47 @@ require 'chef/version'
 
 describe 'poise_boiler/kitchen' do
     environment SPEC_BLOCK_CI: true
+    file 'poise-boiler-test1.gemspec', <<-EOH
+Gem::Specification.new do |spec|
+  spec.name = 'poise-boiler-test1'
+  spec.version = '1.0.0'
+  spec.authors = ['Noah Kantrowitz']
+  spec.email = %w{noah@coderanger.net}
+  spec.description = %q||
+  spec.summary = %q||
+  spec.homepage = 'http://example.com/'
+  spec.license = 'Apache 2.0'
+
+  spec.files = []
+  spec.executables = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  spec.test_files = spec.files.grep(%r{^(test|spec|features)/})
+  spec.require_paths = []
+
+  spec.add_dependency 'halite'
+end
+EOH
     file '.kitchen.yml', <<-EOH
 ---
 #<% require 'poise_boiler' %>
 <%= PoiseBoiler.kitchen %>
-
-suites:
-- name: default
 EOH
 
   context 'with defaults' do
     context 'kitchen list' do
       command 'kitchen list'
-      its(:stdout) { is_expected.to match(/default-ubuntu-1404\s+(Vagrant|Dummy)\s+PoiseSolo\s+(Busser\s+Sftp\s+)?<Not Created>/) }
+      its(:stdout) do
+        is_expected.to match(/default-ubuntu-1404\s+(Vagrant|Dummy)\s+PoiseSolo\s+(Busser\s+Sftp\s+)?<Not Created>/)
+        is_expected.to match(/default-ubuntu-1204\s+(Vagrant|Dummy)\s+PoiseSolo\s+(Busser\s+Sftp\s+)?<Not Created>/)
+        is_expected.to match(/default-centos-6\s+(Vagrant|Dummy)\s+PoiseSolo\s+(Busser\s+Sftp\s+)?<Not Created>/)
+        is_expected.to match(/default-centos-7\s+(Vagrant|Dummy)\s+PoiseSolo\s+(Busser\s+Sftp\s+)?<Not Created>/)
+      end
     end # /context kitchen list
 
     context 'kitchen diagnose' do
       command 'kitchen diagnose'
-      its(:stdout) { is_expected.to match(%r{- curl -L https://chef.io/chef/install.sh | bash -s --$}) }
-      its(:stdout) { is_expected.to match(%r{/opt/chef/embedded/bin/gem\s+install\s+--no-rdoc\s+--no-ri\s+--bindir\s+/tmp/verifier/bin\s+thor\s+busser\s+busser-serverspec}m) }
-      its(:stdout) { is_expected.to include('require_chef_omnibus: true') }
+      its(:stdout) do
+        is_expected.to include('require_chef_omnibus: true')
+      end
     end # /context kitchen diagnose
   end # /context with defaults
 
@@ -46,30 +67,31 @@ EOH
     file '.kitchen.yml', <<-EOH
 ---
 #<% require 'poise_boiler' %>
-<%= PoiseBoiler.kitchen(platforms: 'linux') %>
-
-suites:
-- name: default
+<%= PoiseBoiler.kitchen(platforms: 'centos') %>
 EOH
     command 'kitchen list'
-    its(:stdout) { is_expected.to match(/default-ubuntu-1404\s+(Vagrant|Dummy)\s+PoiseSolo\s+(Busser\s+Sftp\s+)?<Not Created>/) }
-    its(:stdout) { is_expected.to match(/default-ubuntu-1204\s+(Vagrant|Dummy)\s+PoiseSolo\s+(Busser\s+Sftp\s+)?<Not Created>/) }
-    its(:stdout) { is_expected.to match(/default-centos-6\s+(Vagrant|Dummy)\s+PoiseSolo\s+(Busser\s+Sftp\s+)?<Not Created>/) }
-    its(:stdout) { is_expected.to match(/default-centos-7\s+(Vagrant|Dummy)\s+PoiseSolo\s+(Busser\s+Sftp\s+)?<Not Created>/) }
+    its(:stdout) do
+      is_expected.to_not match(/default-ubuntu-1404/)
+      is_expected.to_not match(/default-ubuntu-1204/)
+      is_expected.to match(/default-centos-6\s+(Vagrant|Dummy)\s+PoiseSolo\s+(Busser\s+Sftp\s+)?<Not Created>/)
+      is_expected.to match(/default-centos-7\s+(Vagrant|Dummy)\s+PoiseSolo\s+(Busser\s+Sftp\s+)?<Not Created>/)
+    end
   end # /context with a platform alias
 
   context 'with $CHEF_VERSION set' do
     command 'kitchen diagnose'
     environment CHEF_VERSION: 12
-    its(:stdout) { is_expected.to match(%r{- curl -L https://chef.io/chef/install.sh | bash -s -- -v 12$}) }
-    its(:stdout) { is_expected.to include("require_chef_omnibus: '12'") }
+    its(:stdout) do
+      is_expected.to include("require_chef_omnibus: '12'")
+    end
   end # /context with $CHEF_VERSION set
 
   context 'with $CI set' do
     command 'kitchen diagnose'
     environment CI: true, SPEC_BLOCK_CI: false
-    its(:stdout) { is_expected.to match(%r{- curl -L https://chef.io/chef/install.sh | bash -s -- -v #{Chef::VERSION}$}) }
-    its(:stdout) { is_expected.to include("require_chef_omnibus: #{Chef::VERSION}") }
+    its(:stdout) do
+      is_expected.to include("require_chef_omnibus: #{Chef::VERSION}")
+    end
   end # /context with $CI set
 
   context 'with a platform override' do
@@ -81,12 +103,11 @@ EOH
 platforms:
 - name: gentoo
 - name: arch
-
-suites:
-- name: default
 EOH
     command 'kitchen list'
-    its(:stdout) { is_expected.to match(/default-gentoo\s+(Vagrant|Dummy)\s+PoiseSolo\s+(Busser\s+Sftp\s+)?<Not Created>/) }
-    its(:stdout) { is_expected.to match(/default-arch\s+(Vagrant|Dummy)\s+PoiseSolo\s+(Busser\s+Sftp\s+)?<Not Created>/) }
+    its(:stdout) do
+      is_expected.to match(/default-gentoo\s+(Vagrant|Dummy)\s+PoiseSolo\s+(Busser\s+Sftp\s+)?<Not Created>/)
+      is_expected.to match(/default-arch\s+(Vagrant|Dummy)\s+PoiseSolo\s+(Busser\s+Sftp\s+)?<Not Created>/)
+    end
   end # /context with a platform override
 end
