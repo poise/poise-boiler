de8272b68f1660de291a38347c676f99b2b6c233
Noah Kantrowitz <noah@coderanger.net>
2016-01-31 23:35:14 -0800

    I'm bad at my own code.

--- spec/kitchen_spec.rb
+++ spec/kitchen_spec.rb
@@ -116,7 +116,7 @@ EOH
 #<% require 'poise_boiler' %>
 <%= PoiseBoiler.kitchen(driver: 'rackspace') %>
 EOH
-    env RACKSPACE_USERNAME: 'dummy'
+    environment RACKSPACE_USERNAME: 'dummy'
     command 'kitchen list'
     its(:stdout) do
       is_expected.to match(/default-ubuntu-1204\s+Rackspace\s+PoiseSolo\s+(Busser\s+Sftp\s+)?<Not Created>/)
